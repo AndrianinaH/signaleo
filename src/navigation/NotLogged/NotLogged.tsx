da56b6a2b6e5361c3076d6be82631663bdef1a0e
Andrianina <andrianina@hairun-technology.com>
2020-06-18 09:23:28 +0300

    [reactnative] add animation in signout

--- src/navigation/NotLogged/NotLogged.tsx
+++ src/navigation/NotLogged/NotLogged.tsx
@@ -4,14 +4,26 @@ import LoginScreen from '../../screens/Auth/Login/LoginScreen';
 import SignupScreen from '../../screens/Auth/Signup/SignupScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 
-interface Props {}
+interface Props {
+  isSignout?: boolean;
+}
 
 const Stack = createStackNavigator();
 
 const NotLogged = (props: Props) => {
+  const { isSignout } = props;
   return (
     <Stack.Navigator>
-      <Stack.Screen name={ROUTES.Login} component={LoginScreen} />
+      <Stack.Screen
+        name={ROUTES.Login}
+        component={LoginScreen}
+        options={{
+          title: 'Sign in',
+          // When logging out, a pop animation feels intuitive
+          // You can remove this if you want the default 'push' animation
+          animationTypeForReplace: isSignout ? 'pop' : 'push',
+        }}
+      />
       <Stack.Screen name={ROUTES.Signup} component={SignupScreen} />
     </Stack.Navigator>
   );
